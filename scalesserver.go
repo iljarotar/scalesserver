f54acaba547d8c99202f621fe3af4f442881c817
iljarotar <kontakt@ilja-rotar.de>
2022-01-05 17:52:37 +0100

    Server serves json response

--- scalesserver.go
+++ scalesserver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -19,12 +20,22 @@ func main() {
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm error: %v", err)
+		return
 	}
-	if n, err1 := strconv.Atoi(r.FormValue("n")); err1 != nil {
-		fmt.Fprintln(w, "Invalid input for n")
-	} else if k, err2 := strconv.Atoi(r.FormValue("k")); err2 != nil {
-		fmt.Fprintln(w, "Invalid input for n")
-	} else {
-		fmt.Fprintln(w, scalesalgorithm.GetScales(n, k, k))
+	n, err1 := strconv.Atoi(r.FormValue("n"))
+	k, err2 := strconv.Atoi(r.FormValue("k"))
+	if err1 != nil || err2 != nil {
+		fmt.Fprintln(w, "Invalid input for n or k")
+		return
 	}
+	w.Header().Set("Content-Type", "application/json")
+	scales := scalesalgorithm.GetScales(n, k, k)
+	response := make(map[string][][]int)
+	response["scales"] = scales
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		fmt.Fprintln(w, "JSON error", err)
+	}
+	w.Write(jsonResponse)
+	return
 }
